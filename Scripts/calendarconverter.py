67b8279a667696d1f27ad8086a344417e7198911
BigFood2307 <36566235+BigFood2307@users.noreply.github.com>
2020-01-13 22:46:08 +0100

    Update calendarconverter.py

--- Scripts/calendarconverter.py
+++ Scripts/calendarconverter.py
@@ -9,6 +9,7 @@ switches = []
 states = []
 
 for line in file:
+	#Zeilenweise Analyse der Kalenderdatei
 	cleanline = line.strip()
 	if state == "ready":
 		if cleanline == "BEGIN:VEVENT":
@@ -17,11 +18,13 @@ for line in file:
 			continue
 	if state == "event":
 		if cleanline.startswith("DTSTART:"):
+			#Hier ist das Datum gespeichert, die 2 Wochen Verschiebung für die Anwesenheitssimulation, sowie eine Anpassung an die Zeitzone werden hier beachtet.
 			newdate = datetime.datetime(int(cleanline[8:12]), int(cleanline[12:14]),int(cleanline[14:16]), int(cleanline[17:19]), int(cleanline[19:21]), int(cleanline[21:23])) - datetime.timedelta(weeks=2) + datetime.timedelta(hours=1)
 			dates.append(newdate)
 			#print(newdate.isoformat())
 			continue
 		if cleanline.startswith("DESCRIPTION:send "):
+			#Die Daten, welcher Schater geschalten wurde und welchen Status er nun hat kommen in einem festen Format
 			newswitch = cleanline.split(" ")[1]
 			newstate = (cleanline.split(" ")[2] == "ON")
 			switches.append(newswitch)
@@ -35,6 +38,7 @@ for line in file:
 switchtimes = []
 switchstates = []
 
+#Begrenzung der Daten auf einen Schalter und einen bestimmten Zeitraum
 for i in range(len(dates)):
 	if switches[i] == "EG_WC_SchalterWC1_Power":
 		if dates[i] > datetime.datetime(2019, 10, 12):
@@ -44,12 +48,13 @@ for i in range(len(dates)):
 durations = []
 durationtimes = []
 		
+#Filtern auf Daten, bei denen der Schalter ausgeschaltet wurde.
 for i in range(1, len(switchtimes)):
 	if (not switchstates[i]) and switchstates[i-1]:
 		durations.append(switchtimes[i]-switchtimes[i-1])
 		durationtimes.append(switchtimes[i-1])
 
-#statistics
+#statistics, Wann wurde der Schalter wie oft gedrückt und wie lange war er an
 
 seconds = 0
 

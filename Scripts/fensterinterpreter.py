a3956c5def1601f0b1d0104b0c3dda12b8935389
BigFood2307 <36566235+BigFood2307@users.noreply.github.com>
2020-01-13 22:38:49 +0100

    Update fensterinterpreter.py

--- Scripts/fensterinterpreter.py
+++ Scripts/fensterinterpreter.py
@@ -1,7 +1,9 @@
+#Dieses Script liest die Daten für ein Fenster ein und analysiert sie nach verschiedenen Aspekten
+
 import datetime
 import json
 
-door = "EG_FL_Haustuer"
+door = "EG_FL_Haustuer"		#Der interne Name des Fensterkontakt ist im Namen der Datei enthalten
 
 file = open("..\Data\\" + door + "_Contact.json", "r")
 
@@ -9,10 +11,11 @@ jsontext = file.read()
 
 file.close()
 
-result = json.loads(jsontext)
+result = json.loads(jsontext)			#Umwandeln der Textdaten in Python Datentypen
 values = result["results"][0]["series"][0]["values"]
 
 for v in values:
+	#Das Datum ist noch als String gespeichert, Umwandlung in ein datetime objekt zur besseren behandlung
 	v[0] = datetime.datetime.fromisoformat(v[0].split(".")[0] + "+00:00").astimezone()
 	#print(v[0])
 	
@@ -25,9 +28,11 @@ timedifcnt = 0
 
 for i in range(1, len(values)):
 	if values[i][1] and not values[i-1][1]:# and values[i][0].weekday() < 5:
+		#Nur Daten, zu denen der Wert von 0 (Geschlossen) zu 1 (Offen) gewechselt ist, werden berücksichtigt
 		actualOpens.append(values[i])
 		if(i < len(values)-1):
 			if (values[i+1][0] - values[i][0]).total_seconds() < 3600:
+				#berechnung der durchschnittlichen Öffnungszeit, daten über 1 Stunde werden ignoriert (mit hoher Wahrscheinlichkeit technische Probleme)
 				timedifsum += values[i+1][0] - values[i][0]
 				#print(values[i+1][0] - values[i][0])
 				timedifcnt += 1
@@ -39,6 +44,7 @@ print("Average time opened: " + str(datetime.timedelta(seconds = timedifsum.tota
 		
 print("Number of Opens:" + str(len(actualOpens)))
 
+#Statistische Verteilung über den Tag
 timecounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for ao in actualOpens:
@@ -46,8 +52,7 @@ for ao in actualOpens:
 	
 print(timecounts)
 
-#"""
-
+#Die jeweils erste Öffnung pro Tag wird gesucht
 lastDate = datetime.date.min
 firstOpens = []
 
@@ -70,6 +75,7 @@ print("Average Time: " + str(datetime.timedelta(seconds=secondsAvg)))
 testDate = firstOpens[0][0].date()
 opened = False
 
+#Tage, an denen das Fenster morgens nicht geöffnet wurde, werden gesucht
 unopenedMorningDays = []
 
 i = 0
@@ -93,6 +99,7 @@ for umd in unopenedMorningDays:
 testDate = actualOpens[0][0].date()
 opened = False
 
+#Nochmal für den gesamten Tag
 unopenedDays = []
 
 i = 0
@@ -110,4 +117,3 @@ while testDate <= actualOpens[-1][0].date() and i < len(actualOpens):
 		
 print("Days without opening all day: " + str(len(unopenedDays)))
 
-#"""
